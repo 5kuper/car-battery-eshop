c36b51bf8ace582c46e1ee146bac5132fe4576b3
Dmitry Polyukhovich <dm.pol@yahoo.com>
2025-05-10 13:36:50 +0300

    Fix gitignore

---
+++ BatteriesAPI/BattAPI.App/Services/Abstractions/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using BattAPI.Domain.Entities;
+
+namespace BattAPI.App.Services.Abstractions
+{
+    public record AuthOptions(string Secret);
+
+    public interface IAuthService
+    {
+        public string GenerateToken(User user);
+    }
+}
---
+++ BatteriesAPI/BattAPI.App/Services/Implementations/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using BattAPI.App.Services.Abstractions;
+using BattAPI.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BattAPI.App.Services.Implementations
+{
+    public class AuthService(AuthOptions opt) : IAuthService
+    {
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(opt.Secret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
